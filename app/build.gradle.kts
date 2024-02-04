d02fcb15bb651c1548ddb765825856aeb5171394
ztftrue <ztftrue@gmail.com>
2024-02-04 22:56:32 +0800

    update: enable Minify Shrink

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
