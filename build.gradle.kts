4630a8beaaf3abe8392fb796dd7f1af7d97b8cd0
ztftrue <ztftrue@gmail.com>
2025-11-23 00:02:15 +0800

    Update build tools and plugin versions
    This commit updates the versions of the Android Gradle Plugin, Kotlin plugins, and the Gradle wrapper.
    
    Key changes:
    - Android Gradle Plugin version updated from 8.9.2 to 8.12.3.
    - Kotlin Android plugin version updated from 2.1.20 to 2.2.21.
    - Kotlin Compose and Serialization plugins updated from 2.1.0 to 2.2.21.
    - Gradle wrapper distribution URL updated from gradle-8.11.1-bin.zip to gradle-8.13-bin.zip.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.9.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("com.android.application") version "8.12.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("com.google.devtools.ksp") version "2.1.20-2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
 }
