9a54eb8072b185c0636e63c78043b2d5e291a9ab
ztftrue <ztftrue@gmail.com>
2025-12-06 23:28:46 +0800

    Chore: Bump KSP version
    This commit updates the version of the `com.google.devtools.ksp` plugin from `2.1.20-2.0.0` to `2.2.21-2.0.4`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.12.3" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
-    id("com.google.devtools.ksp") version "2.1.20-2.0.0" apply false
+    id("com.google.devtools.ksp") version "2.2.21-2.0.4" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
 }
