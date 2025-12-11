b5669f8ae28f4b34ab67556357d88ea8a0828a8c
ztftrue <ztftrue@gmail.com>
2025-12-11 19:50:05 +0800

    Chore: Bump version to 0.1.60
    This commit increments the app version in `app/build.gradle.kts`.
    - `versionCode` is updated from 59 to 60.
    - `versionName` is updated from "0.1.59" to "0.1.60".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ztftrue.music"
         minSdk = 30
         targetSdk = 36
-        versionCode = 59
-        versionName = "0.1.59"
+        versionCode = 60
+        versionName = "0.1.60"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
