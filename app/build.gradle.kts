1d2d28e7a367b0cfe35a9238056942c264bd5fa4
ztftrue <ztftrue@gmail.com>
2026-09-26 13:09:17 +0800

    Chore: Bump application version to 0.1.76
    This commit updates the application version metadata in the build configuration to prepare for a new release.
    
    - **`build.gradle.kts`**:
        - Incremented `versionCode` from `74` to `76`.
        - Updated `versionName` from `"0.1.74"` to `"0.1.76"`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ztftrue.music"
         minSdk = 30
         targetSdk = 37
-        versionCode = 74
-        versionName = "0.1.74"
+        versionCode = 76
+        versionName = "0.1.76"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
